93b58536bcfff19fab3c740d84a8c8bbb3230591
Dmytro Shvets <ddimaschvets@gmail.com>
2025-11-09 16:25:21 +0200

    added link to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router";
+
 export default function Header() {
   return (
     <header className="bg-primary  ">
       <div className="container w-full font-bold text-xl text-white">
-        Star Wars Explorer
+        <Link to="/">Star Wars Explorer</Link>
       </div>
     </header>
   );
